ff4c2063eb22ca4008de1a3c747602569a8b1e02
Sashank Merugumuvvala <56181878+Sash9@users.noreply.github.com>
2025-01-17 16:55:05 -0500

    Add files via upload

---
+++ user_data.pl
@@ -0,0 +1,72 @@
+% Collected Data
+name('john').
+age('john', 21).
+gender('john', 'M').
+resident('john', 'Y').
+transfer('john', 'Y').
+type('john', 'U').
+year('john', 1).
+school('john', 4).
+gpa('john', 3.5).
+monthly_budget('john', 5000.0).
+percent_food('john', 1500.0).
+percent_transportation('john', 1000.0).
+percent_entertainment('john', 1000.0).
+percent_savings('john', 1500.0).
+monthly_food('john', 1500.0).
+monthly_transportation('john', 800.0).
+monthly_entertainment('john', 500.0).
+monthly_savings('john', 2200.0).
+tuition_per_sem('john', 8000.0).
+rent('john', 2000.0).
+meal_plan_price('john', 900.0).
+ideal_college_expense('john', 10000.0).
+scholarships('john', 2500.0).
+% Collected Data
+name('bob').
+age('bob', 19).
+gender('bob', 'M').
+resident('bob', 'N').
+transfer('bob', 'N').
+type('bob', 'G').
+year('bob', 1).
+school('bob', 6).
+gpa('bob', 2.9).
+monthly_budget('bob', 5000.0).
+percent_food('bob', 1500.0).
+percent_transportation('bob', 1000.0).
+percent_entertainment('bob', 1000.0).
+percent_savings('bob', 1500.0).
+monthly_food('bob', 2000.0).
+monthly_transportation('bob', 1500.0).
+monthly_entertainment('bob', 1230.0).
+monthly_savings('bob', 500.0).
+tuition_per_sem('bob', 5000.0).
+rent('bob', 2000.0).
+meal_plan_price('bob', 1000.0).
+ideal_college_expense('bob', 6000.0).
+scholarships('bob', 0.0).
+% Collected Data
+name('alice').
+age('alice', 20).
+gender('alice', 'F').
+resident('alice', 'Y').
+transfer('alice', 'N').
+type('alice', 'U').
+year('alice', 4).
+school('alice', 3).
+gpa('alice', 4.0).
+monthly_budget('alice', 5000.0).
+percent_food('alice', 1500.0).
+percent_transportation('alice', 1000.0).
+percent_entertainment('alice', 1000.0).
+percent_savings('alice', 1500.0).
+monthly_food('alice', 1800.0).
+monthly_transportation('alice', 200.0).
+monthly_entertainment('alice', 500.0).
+monthly_savings('alice', 2000.0).
+tuition_per_sem('alice', 7000.0).
+rent('alice', 2000.0).
+meal_plan_price('alice', 800.0).
+ideal_college_expense('alice', 9000.0).
+scholarships('alice', 1500.0).
